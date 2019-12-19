517001f9f36e2bbbe3cf3d6625f3d0f84573420e
Myscya <myscya@users.noreply.github.com>
2019-12-19 09:45:31 +0100

    Update ServletModificaEnteET.java

--- src/controller/ServletModificaEnteET.java
+++ src/controller/ServletModificaEnteET.java
@@ -13,7 +13,7 @@ import model.EnteConvenzionato;
 /**
  * Servlet implementation class ServletRegistazioneEnteET.
  */
-@WebServlet("/ServletRegistazioneEnteET")
+@WebServlet("/ServletModificaEnteET")
 public class ServletModificaEnteET extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
