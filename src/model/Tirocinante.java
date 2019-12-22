268ca50467b8255dab4dede400c8ec3e3bdfe8d1
AleSer <a.serritiello7@studenti.unisa.it>
2019-12-22 11:58:47 +0100

    Aggiunto TirocinanteDAOTest, aggiornati Bean e DAO Tirocinante

--- src/model/Tirocinante.java
+++ src/model/Tirocinante.java
@@ -8,6 +8,12 @@ import java.util.Date;
  * Questa classe va a rappresentare l'oggetto Tirocinante che nel seguente dominio applicativo raffigura il tirocinante che si presta al Tirocinio.
  */
 public class Tirocinante extends Student {
+	private String email;
+	private String name;
+	private String surname;
+	private char sex;
+	private String password;
+	private int userType;
     private int matricola;
     private Date dataNascita;
     private String luogoNascita;
@@ -15,8 +21,8 @@ public class Tirocinante extends Student {
     private String residenza;
     private String codiceFiscale;
     private long telefono;
-    private String email;
-    /**
+    
+	/**
      * Contructor.
      *
      * @param matricola is the id of Tirocinante
@@ -27,19 +33,67 @@ public class Tirocinante extends Student {
      * @param codiceFiscale is the tax code of the Tirocinante.
      * @param telefono is the number phone of the Tirocinante.
      */
-    public Tirocinante(int matricola, Date dataNascita, String luogoNascita, String cittadinanza, String residenza, String codiceFiscale, long telefono, String email) {
-        this.matricola = matricola;
+    public Tirocinante(String email, String name, String surname, char sex, String password,
+    	      int userType, int matricola, Date dataNascita, String luogoNascita, String cittadinanza, String residenza, String codiceFiscale, long telefono) {
+    	this.email = email;
+    	this.name = name;
+        this.surname = surname;
+        this.sex = sex;
+        this.password = password;
+        this.userType = userType;
+    	this.matricola = matricola;
         this.dataNascita = dataNascita;
         this.luogoNascita = luogoNascita;
         this.cittadinanza = cittadinanza;
         this.residenza = residenza;
         this.codiceFiscale = codiceFiscale;
         this.telefono = telefono;
-        this.email = email;
+        
     }
 
 	public Tirocinante() {
     }
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public char getSex() {
+		return sex;
+	}
+
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getUserType() {
+		return userType;
+	}
+
+	public void setUserType(int userType) {
+		this.userType = userType;
+	}
+
 
     public int getMatricola() {
         return matricola;
