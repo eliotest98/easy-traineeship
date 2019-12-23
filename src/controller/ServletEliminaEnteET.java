1ab4a8aaf80f4fa77b39fa2c2b7a2c322f89d211
luigibarbato <luigibarbato0@gmail.com>
2019-12-23 20:10:49 +0100

    ServletEliminaEnteET added,
    Da completare.

---
+++ src/controller/ServletEliminaEnteET.java
@@ -0,0 +1,58 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.EnteConvenzionato;
+import model.DAO.EnteConvenzionatoDAO;
+
+/**
+ * Servlet implementation class ServletEliminaEnteET
+ */
+@WebServlet("/ServletEliminaEnteET")
+public class ServletEliminaEnteET extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletEliminaEnteET() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		EnteConvenzionatoDAO enteDao = new EnteConvenzionatoDAO();
+		EnteConvenzionato enteBean = null;
+		
+		enteBean = (EnteConvenzionato) request.getAttribute("ente");
+		
+		if(enteBean!=null) {
+			try {
+				boolean res = enteDao.eliminaEnte(enteBean);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
