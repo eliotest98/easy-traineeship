ac2757c98f4955d0ee72401545def27935038117
AleSer <57143788+Lexacus894@users.noreply.github.com>
2020-01-11 14:27:32 +0100

    Update ServletUploadET.java

--- src/controller/ServletUploadET.java
+++ src/controller/ServletUploadET.java
@@ -5,6 +5,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URL;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -66,8 +68,7 @@ public class ServletUploadET extends HttpServlet {
         	pdfPath =  "pdf" + builder.toString() + fileName;
         }
  
-        String filePath = "C:\\Users\\alex8\\Documents\\GitHub\\easy-traineeship\\ProgettoFormativo\\"+pdfPath;
-        
+        String filePath = "C:\\Users\\" + System.getProperty("user.name")+"\\Documents\\GitHub\\easy-traineeship\\ProgettoFormativo\\"+pdfPath;
         OutputStream os = null;
         
         try {
