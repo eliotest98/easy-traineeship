14249d9703d1bc0113cb4af1d5e402e4af14da30
AleSer <57143788+Lexacus894@users.noreply.github.com>
2020-01-12 23:27:50 +0100

    Update ServletDownloadETTest.java

--- src/test/testET/ServletDownloadETTest.java
+++ src/test/testET/ServletDownloadETTest.java
@@ -8,8 +8,6 @@ import java.io.InputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,47 +15,50 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.Part;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletResponse;
 
-import controller.DbConnection;
+
 import controller.ServletDownloadET;
-import controller.ServletProgettoFormativoET;
-import controller.ServletUploadET;
-import model.Student;
 import model.Tirocinio;
-import model.DAO.TirocinioDAO;
+
 
 	
 
 class ServletDownloadETTest {
 	
 	HttpServletRequest requestMock = mock(HttpServletRequest.class);
-	//HttpServletResponse responseSpy = spy(HttpServletResponse.class);
 	MockHttpServletResponse responseMock = new MockHttpServletResponse();
 	HttpSession sessionMock = mock(HttpSession.class);
 	RequestDispatcher dispatcherMock = mock(RequestDispatcher.class);
 	ServletDownloadET test = new ServletDownloadET();
 	InputStream is = mock(InputStream.class);
-	
+	File prova  = new File("C:\\Users\\" + System.getProperty("user.name") + "\\Documents\\GitHub\\easy-traineeship\\ProgettoFormativo\\test.pdf");
 	
 	//Metodo setUp()
 	@BeforeEach 
 	void setUp() {
 		when(requestMock.getSession()).thenReturn(sessionMock);
 		when(sessionMock.getAttribute("userET")).thenReturn("3");
-		//when(sessionMock.getAttribute("user")).thenReturn(user);
+		try {
+			prova.createNewFile();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Metodo tearDown()
+	@AfterEach
+	void tearDown() {
+		prova.delete();
 	}
 	
 	@Test
-	void testBoh() {
+	void testDownloadET() {
 		try {
-			//ServletOutputStream sos = response.getOutputStream();
-			//when(responseMock.getOutputStream()).thenReturn(responseMock.);
 			Tirocinio test1=new Tirocinio();
 			test1.setCodTirocinio(1);
 			test1.setDataInizioTirocinio("1999-12-12");                  
@@ -66,12 +67,13 @@ class ServletDownloadETTest {
 			test1.setCompetenzeAcquisire("javaScript");
 			test1.setAttivitaPreviste("java");
 			test1.setSvolgimentoTirocinio("Bene");
-			test1.setProgettoFormativo("pdf8HW7XMXWC31009.pdf");
+			test1.setProgettoFormativo("test.pdf");
 			test1.setDescrizioneEnte("Ragazzo valido");
 			test1.setMatricola(4859);
 			test1.setPartitaIva("11111111111");
 			when(sessionMock.getAttribute("Tirocinio")).thenReturn(test1);
 			test.doPost(requestMock, responseMock);
+			assertEquals(prova.exists(),true);
 		}
 		catch (Exception e) {
 			e.printStackTrace();
